cabe93a2a5a7efe3ccedf75968f2f479957da12b
Christopher Phillips <cphillips918@gmail.com>
2016-09-19 15:19:03 -0400

    fixed elements from clearing to removing click functionality

--- unit-project/main.js
+++ unit-project/main.js
@@ -4,39 +4,49 @@ var cardsToRemove = [];
 
 //resets the board without having to reload the page
 //will always run first when button clicked so that cardTypes is empty and can be re-made
+
 function clearBoard() {
-    var board = document.getElementById('game-board');
-    //http://stackoverflow.com/questions/14555214/remove-all-divs-from-in-a-parent-div//
-    while(board.firstChild) {
-        board.removeChild(board.firstChild);
-    }
-    cardTypes.length = 0;
+  var board = document.getElementById('game-board');
+  //http://stackoverflow.com/questions/14555214/remove-all-divs-from-in-a-parent-div//
+  while(board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
+  cardTypes.length = 0;
 }
 
 //makes it so that cardTypes is set up dynamically and randomly when button clicked
 //can be fleshed out to incorporate as many values and cards as you would want (switch functionality)
 //also think about customization for more matching options
+//could abstract the for loop into a generate board function - right now this makes the possibility of there being a board with not enough matches
+
 function createRandomCards() {
-    var numberOfCards = document.getElementsByName("quantity")[0].value;
+  var numberOfCards = document.getElementsByName("quantity")[0].value;
+  if (numberOfCards % 2 != 0){
+    alert('Card number has to be even!')
+  }else{
     for(i=0;i<numberOfCards;i++) {
-        var random = Math.floor(Math.random()*2)
-        if(random === 0) {
-            cardTypes.push('King');
-        }else{
-            cardTypes.push('Queen');
-        }
+      var random = Math.floor(Math.random()*2)
+      if(random == 0) {
+        cardTypes.push('King');
+      }else{
+        cardTypes.push('Queen');
+      }
     }
+  }
 }
 
 //pushes clicked item's id into an array for use later
+
 function pushToRemove () {
     cardsToRemove.push(this.id);
 }
 //resets cardsToRemove array so that it's always blank when running a match
+
 function clearRemoveArray(){
     cardsToRemove =[];
 }
-//keeps cardTypes array updated so that cards in play = length of the array
+//keeps cardTypes array updated so that cards in play = length of the array; Some functions depending on this value being the same
+
 function updateCardTypesArray(){
     cardTypes.length = cardTypes.length-2;
 }
@@ -58,10 +68,9 @@ function createBoard () {
         document.getElementsByClassName('card')[i].addEventListener('click', pushToRemove);
         document.getElementsByClassName('card')[i].addEventListener('click', isTwoCards);
         //maybe add event listener for removing the element after a match - ask people//
+        //this all needs to be refactored so flipCard pushToRemove and isTwoCards are stacked into one function for one click element
     }
 }
-
-
 
 //Game Logic!! referenced as on click functionality for the dynamic elements created in createBoard function
 //checks for match of values stored in data-type that has been pushed into cardsInPlay array
@@ -69,15 +78,14 @@ function createBoard () {
 //if else resets the innerHTML so card image is just updated back to the back facing card
 function isMatch(cardsInPlay){
     if (cardsInPlay[0] === cardsInPlay[1]){
-        alert('Great Match!');
         for(i=0;i<cardsToRemove.length;i++) {
-            document.getElementById(parseInt(cardsToRemove[i])).className='';
-            document.getElementById(parseInt(cardsToRemove[i])).innerHTML='';
+            document.getElementById(parseInt(cardsToRemove[i])).removeEventListener('click', flipCard);
+            document.getElementById(parseInt(cardsToRemove[i])).removeEventListener('click', pushToRemove);
+            document.getElementById(parseInt(cardsToRemove[i])).removeEventListener('click', isTwoCards);
         }
         clearRemoveArray();
         updateCardTypesArray();
     }else{
-        alert('No Match No Dice');
         for(i=0;i<cardTypes.length;i++) {
             document.getElementsByClassName('card')[i].innerHTML = '';
         }clearRemoveArray();
@@ -112,5 +120,3 @@ function setUpButton() {
 }
 //You now have a button that does something!
 setUpButton();
-
-/* There is an issue where isMatch is run on the else side of the if statment. The function is trying to act on cardTypes.Length but since we have removed some of the objects referenced there it cannot run the command and breaks the code. Have to figure out a way to dynamically update cardTypes as things are removed*/
